ec583441b75073a984a437b8d08b42aeef35dcd6
Brett Bieber <brett.bieber@gmail.com>
2009-07-07 21:21:12 +0000

    Update readme with details on setup, add includes dir to the sample config.

--- config.sample.php
+++ config.sample.php
@@ -1,4 +1,4 @@
 <?php
 // set up basics
-set_include_path(dirname(__FILE__).'/src'.PATH_SEPARATOR.get_include_path());
+set_include_path(dirname(__FILE__).'/src'.PATH_SEPARATOR.dirname(__FILE__).'/includes/php');
 ?>
