c408b7426d13530fd06e78166bbe673c2bfce08d
Brett Bieber <brett.bieber@gmail.com>
2009-12-05 16:31:43 +0000

    Update readme with some better instructions, and note that SimpleChannelFrontend is a dependency. Update sample config to match what is said in the README.

--- config.sample.php
+++ config.sample.php
@@ -1,5 +1,7 @@
 <?php
+ini_set('display_errors', true);
+error_reporting(E_ALL);
 // set up basics
-require_once dirname(__FILE__).'/../PEAR2/autoload.php';
+require_once dirname(__FILE__).'/includes/autoload.php';
 
 pear2\SimpleChannelFrontend\Main::$url = 'http://pear2.php.net/';
