269c3bc92429bd2d5012880a24e9bcbe84a8556d
Brett Bieber <brett.bieber@gmail.com>
2009-12-06 19:31:37 +0000

    improve news page. Package pages - remove channel-discover step since this is a default channel, add version number to install step.

--- templates/pear2/News.tpl.php
+++ templates/pear2/News.tpl.php
@@ -4,13 +4,22 @@ $parent->context->page_title = 'News | '.pear2\SimpleChannelFrontend\Main::$chan
 ?>
 <div id="news">
     <div class="grid_8 left">
-    <h2>Latest News</h2>
-    <p>Welcome to the next generation of PEAR.</p>
-    <p>Users can get started by reading
-    <a href="http://pear.php.net/manual/en/installationpyrus.introduction.php">the introduction</a>.</p>
+        <h2>Welcome to the next generation of PEAR</h2>
+        <ol class="instructions">
+            <li>Download the new PEAR installer, pyrus.phar<br />
+                <code><a href="http://svn.php.net/viewvc/pear2/Pyrus/trunk/pyrus.phar?view=co">pyrus.phar</a></code></li>
+            <li>Install packages<br />
+                <code>$>php pyrus.phar install pear/Net_URL</code></li>
+        </ol>
+        <p>Users can get started by reading
+        <a href="http://pear.php.net/manual/en/installationpyrus.introduction.php">the introduction</a>.</p>
     </div>
     <div class="grid_4 right">
-        <h3>Download Pyrus</h3>
-        <a href="http://svn.php.net/viewvc/pear2/Pyrus/trunk/pyrus.phar?view=co">Download pyrus.phar</a>
+        <h3>Download</h3>
+        <h4><a href="http://svn.php.net/viewvc/pear2/Pyrus/trunk/pyrus.phar?view=co">Download pyrus.phar</a></h4>
+        <p>
+          This website provides packages to install using the pyrus package installer.
+          With pyrus you can install all the packages available on this channel, as well as any PEAR
+          compatible package from a large number of repositories.</p>
     </div>
 </div>
---
+++ templates/pear2/Package.tpl.php
@@ -0,0 +1,34 @@
+<?php
+// Set the title for the main template
+$parent->context->page_title = $context->name.' | '.pear2\SimpleChannelFrontend\Main::$channel->name;
+?>
+<div class="package">
+    <div class="grid_8 left">
+        <h2>Package :: <?php echo $context->name; ?></h2>
+        <p>
+            <?php
+            echo nl2br(trim($context->description));
+            ?>
+        </p>
+        <h3>Installation</h3>
+        <ol class="instructions">
+            <?php
+            $latest = $context->__raw('version');
+            ?>
+            <li><code>$>php pyrus.phar install <?php echo $context->name . '-' . $latest['release']; ?></code></li>
+        </ol>
+    </div>
+    <div class="grid_4 right">
+        <h3>Releases</h3>
+        <ul>
+            <?php
+             foreach ($context as $version => $release): ?>
+            <li>
+                <?php echo $version; ?> <span class="stability"><?php echo $release['stability']; ?></span>
+                <abbr class="releasedate" title="<?php echo $context->date.' '.$context->time; ?>"><?php echo $context->date; ?></abbr>
+                <a class="download" href="<?php echo $context->getDownloadURL('.tgz'); ?>">Download</a>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</div>
