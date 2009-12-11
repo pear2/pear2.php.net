93bc8c14955467e0c3610400ec028500d53bac86
Amir Mohammad Saied <amirsaied@gmail.com>
2009-12-11 18:37:26 +0000

    Styles and templates for releases page

--- templates/pear2/Package.tpl.php
+++ templates/pear2/Package.tpl.php
@@ -22,7 +22,8 @@ $parent->context->page_title = $context->name.' | '.pear2\SimpleChannelFrontend\
             <?php
              foreach ($context as $version => $release): ?>
             <li>
-                <?php echo $version; ?> <span class="stability"><?php echo $release['stability']; ?></span>
+                <a href="<?php echo pear2\SimpleChannelFrontend\Main::getURL() . $context->name . '-' . $version; ?>"><?php echo $version; ?></a>
+                <span class="stability"><?php echo $release['stability']; ?></span>
                 <abbr class="releasedate" title="<?php echo $context->date.' '.$context->time; ?>"><?php echo $context->date; ?></abbr>
                 <a class="download" href="<?php echo $context->getDownloadURL('.tgz'); ?>">Download</a>
             </li>
