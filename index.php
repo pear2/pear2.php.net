828199ab3dbd336b64f749499e9aa41d39dc3742
Amir Mohammad Saied <amirsaied@gmail.com>
2009-12-11 18:31:41 +0000

    Get the version number as well as the package name from URI

--- index.php
+++ index.php
@@ -4,9 +4,15 @@ require_once 'config.inc.php';
 $options = $_GET;
 
 if (isset($_SERVER['REDIRECT_URL'])) {
-    $options['view']    = 'package';
-    $options['package'] = substr($_SERVER['REDIRECT_URL'],
-                                 strrpos($_SERVER['REDIRECT_URL'], '/'));
+    $options['view'] = 'package';
+    preg_match('/\/(?<package>[0-9a-z_]+)(-(?<version>[0-9ab.]+))?$/i',
+        $_SERVER['REDIRECT_URL'], $matches);
+    $options['package'] = $matches['package'];
+
+    if (isset($matches['version'])) {
+        $options['packageVersion'] = $matches['version'];
+        $options['view']           = 'release';
+    }
 }
 
 $channel = new \pear2\Pyrus\ChannelFile(__DIR__ . '/channel.xml');
