5126d658ec44e8087f1e63bd58e7c66f398edd98
Brett Bieber <brett.bieber@gmail.com>
2010-06-01 19:27:04 +0000

    Use a black, console looking pearbox for the install instructions.

--- templates/pear2/InstallInstructions.tpl.php
+++ templates/pear2/InstallInstructions.tpl.php
@@ -1,4 +1,10 @@
-<h3>Installation</h3>
-<ol class="instructions">
-    <li><code>$>php pyrus.phar install <?php echo $context; ?></code></li>
-</ol>
+<div class="pearbox instructions">
+    <div class="pearbox-header">
+        <h2>Installation</h2>
+    </div>
+    <div class="pearbox-content">
+        <ol>
+            <li><code>$>php pyrus.phar install <?php echo $context; ?></code></li>
+        </ol>
+    </div>
+</div>
