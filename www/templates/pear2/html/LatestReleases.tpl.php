957fc1d0e8c530b3e1f7c8ab370e42afcff07df8
Michael Gauthier <gauthierm@php.net>
2010-06-15 21:10:47 +0000

    Display recent packages in newspaper column order on PEAR2.

--- www/templates/pear2/html/LatestReleases.tpl.php
+++ www/templates/pear2/html/LatestReleases.tpl.php
@@ -1,4 +1,38 @@
 <?php
-foreach ($context as $date=>$package) {
-    echo '<li><a href="'.PEAR2\SimpleChannelFrontend\Main::getURL().$package->name.'-'.$package->version['release'].'">'.str_replace("PEAR2_", "", $package->name).'-'.$package->version['release'].'<span> — '.date('F j', strtotime($date)).'</span></a></li>';
-}?>
+
+echo '    <ul id="recent-left">' . "\n";
+
+$count = 0;
+
+foreach ($context as $date => $package) {
+
+    if ($count === 5) {
+        echo '    </ul>' . "\n";
+        echo '    <ul id="recent-right">' . "\n";
+    }
+
+    $packageURL = PEAR2\SimpleChannelFrontend\Main::getURL()
+        . $package->name . '-' . $package->version['release'];
+
+    $packageTitle = htmlspecialchars(
+        str_replace('PEAR2_', '', $package->name)
+    );
+
+    $packageTitle .= '-' . $package->version['release'];
+
+    $releaseDate = date('F j', strtotime($date));
+
+    echo '        <li>';
+    echo '<a href="' . $packageURL . '">';
+    echo $packageTitle;
+    echo '<span> — ' . $releaseDate .'</span>';
+    echo '</a>';
+    echo '</li>' . "\n";
+
+    $count++;
+
+}
+
+echo '    </ul>' . "\n";
+
+?>
