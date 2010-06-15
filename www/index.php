75d4bd9ead3dd7d315a78268b5908ffd88af391d
Michael Gauthier <gauthierm@php.net>
2010-06-15 19:32:22 +0000

    Coding standards for pear2web bootstrap.

--- www/index.php
+++ www/index.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once dirname(__FILE__).'/../config.inc.php';
 require_once dirname(__FILE__).'/../src/PEAR2Web/Router.php';
 
@@ -9,12 +10,21 @@ $options = $_GET + PEAR2Web\Router::getRoute($_SERVER['REQUEST_URI']);
 $frontend = new PEAR2\SimpleChannelFrontend\Main($channel, $options);
 
 $savant = new PEAR2\Templates\Savant\Main();
-$savant->setClassToTemplateMapper(new PEAR2\SimpleChannelFrontend\TemplateMapper);
-$savant->setTemplatePath(array(__DIR__ . '/templates/default/html', __DIR__ . '/templates/pear2/html'));
+$savant->setClassToTemplateMapper(
+    new PEAR2\SimpleChannelFrontend\TemplateMapper
+);
+$savant->setTemplatePath(
+    array(
+        __DIR__ . '/templates/default/html',
+        __DIR__ . '/templates/pear2/html'
+    )
+);
 
 switch($frontend->options['format']) {
 case 'rss':
-    $savant->addTemplatePath(__DIR__.'/templates/default/'.$frontend->options['format']);
+    $savant->addTemplatePath(
+        __DIR__ . '/templates/default/' . $frontend->options['format']
+    );
     break;
 }
 
