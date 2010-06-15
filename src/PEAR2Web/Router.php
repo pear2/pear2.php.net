895a6dca8136d6503717a349da66f52da178509a
Michael Gauthier <gauthierm@php.net>
2010-06-15 02:04:19 +0000

    Coding standards.

--- src/PEAR2Web/Router.php
+++ src/PEAR2Web/Router.php
@@ -6,43 +6,48 @@ class Router
 {
     public static function getRoute($requestURI)
     {
-        if (!empty($_SERVER['QUERY_STRING'])) {
-            $requestURI = substr($requestURI, 0, strlen($_SERVER['QUERY_STRING'])*-1-1);
-        }
-
-        $base   = \PEAR2\SimpleChannelFrontend\Main::getURL();
-        $q_base = preg_quote($base, '/');
-
         $options = array();
 
-        switch(true) {
-            case ($requestURI == $base):
-                // Short circuit
-                $options['view'] = 'news';
-                break;
-            case ($requestURI == $base . 'categories/'):
-                $options['view'] = 'categories';
-                break;
-            case ($requestURI == $base . 'packages/'):
-                $options['view'] = 'packages';
-                break;
-            case ($requestURI == $base . 'support/'):
-                $options['view'] = 'support';
-                break;
+        if (!empty($_SERVER['QUERY_STRING'])) {
+            $requestURI = substr(
+                $requestURI,
+                0,
+                -strlen($_SERVER['QUERY_STRING']) - 1
+            );
+        }
 
-            case preg_match('/'.$q_base.'(?<package>[0-9a-z_]+)(-(?<version>[0-9ab.]+))?$/i',
-                    $requestURI, $matches):
+        $base       = \PEAR2\SimpleChannelFrontend\Main::getURL();
+        $quotedBase = preg_quote($base, '/');
+        $packageExp = "/"
+            . "^"
+            . $quotedBase . "            # base"
+            . "(?<package>[0-9a-z_]+)    # package name"
+            . "(-                        # dash separator"
+            . "    (?<version>[0-9ab.]+) # version"
+            . ")?                        # ... is optional"
+            . "$"
+            . "/xi";
 
-                // Viewing an individual package
-                $options['view']    = 'package';
-                $options['package'] = $matches['package'];
+        if ($requestURI === $base) {
+            $options['view'] = 'news';
+        } elseif ($requestURI === $base . 'categories/') {
+            $options['view'] = 'categories';
+        } elseif ($requestURI === $base . 'packages/') {
+            $options['view'] = 'packages';
+        } elseif ($requestURI === $base . 'support/') {
+            $options['view'] = 'support';
+        } elseif (preg_match($packageExp, $requestURI, $matches) === 1) {
 
-                // Release selected
-                if (isset($matches['version'])) {
-                    $options['view']           = 'release';
-                    $options['packageVersion'] = $matches['version'];
-                }
-                break;
+            // Viewing an individual package
+            $options['view']    = 'package';
+            $options['package'] = $matches['package'];
+
+            // Release selected
+            if (isset($matches['version'])) {
+                $options['view']           = 'release';
+                $options['packageVersion'] = $matches['version'];
+            }
+
         }
 
         return $options;
