af917329a2a5834901677b2ab8cf089fe4755a10
Michael Gauthier <gauthierm@php.net>
2010-06-17 17:15:09 +0000

    UTF-8

--- www/templates/pear2/html/PackageList.tpl.php
+++ www/templates/pear2/html/PackageList.tpl.php
@@ -19,7 +19,7 @@ $parent->context->page_title = 'Packages | '.PEAR2\SimpleChannelFrontend\Main::$
 </div>
 <div class="pear-message">
     <div class="pear-message-content">
-        <h3>Don't see what you need?</h3>
+        <h3>Don’t see what you need?</h3>
         <p>Many of the PEAR packages you know and love are still available on <a href="http://pear.php.net/packages.php">pear.php.net</a> or <a href="http://pear.php.net/channels/">other PEAR compatible channels</a>.</p>
     </div>
 </div>
