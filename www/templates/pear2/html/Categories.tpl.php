66c999f072372492455a16f960828926afd9589b
Michael Gauthier <gauthierm@php.net>
2010-06-17 17:07:10 +0000

    Message box styles.

--- www/templates/pear2/html/Categories.tpl.php
+++ www/templates/pear2/html/Categories.tpl.php
@@ -70,5 +70,9 @@ foreach ($context as $category) {
 
             </div>
         </div>
-        <h3>Don't see what you need?</h3>
-        <p>Many of the PEAR packages you know and love are still available on <a href="http://pear.php.net/packages.php">pear.php.net</a> or <a href="http://pear.php.net/channels/">other PEAR compatible channels</a>.</p>
+        <div class="pear-message">
+            <div class="pear-message-content">
+                <h3>Don’t see what you need?</h3>
+                <p>Many of the PEAR packages you know and love are still available on <a href="http://pear.php.net/packages.php">pear.php.net</a> or <a href="http://pear.php.net/channels/">other PEAR compatible channels</a>.</p>
+            </div>
+        </div>
