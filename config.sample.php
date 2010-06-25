860eedb17b8bda2c04edc661d6cbe442c1605ed2
Michael Gauthier <gauthierm@php.net>
2010-06-25 22:23:36 +0000

    Add title to sample configuration.

--- config.sample.php
+++ config.sample.php
@@ -4,4 +4,5 @@ error_reporting(E_ALL);
 // set up basics
 require_once dirname(__FILE__).'/includes/autoload.php';
 
-PEAR2\SimpleChannelFrontend\Main::$url = 'http://pear2.php.net/';
+PEAR2\SimpleChannelFrontend\Main::$url   = 'http://pear2.php.net/';
+PEAR2\SimpleChannelFrontend\Main::$title = 'PEAR2';
