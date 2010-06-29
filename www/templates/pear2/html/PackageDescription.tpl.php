c24a9304d3bee20bd29d6399aaeaf2daf63f6e5b
Michael Gauthier <gauthierm@php.net>
2010-06-29 18:32:19 +0000

    Start using the new kick-ass markdown package to format package descriptions and release notes. Still needs a fair bit of work before merging to live.

--- www/templates/pear2/html/PackageDescription.tpl.php
+++ www/templates/pear2/html/PackageDescription.tpl.php
@@ -1,13 +1,16 @@
             <p>
                 <?php
-                $description = preg_replace("|\&lt\;\?php(.*)\?\&gt\;|Use", "highlight_string('<?php'.html_entity_decode('\\1').'?>', true)", $context->description);
-                echo nl2br(trim($description));
+                $markdown = new \PEAR2\Text\Markdown_Extra();
+                echo $markdown->transform($context->description);
                 ?>
             </p>
 
             <div class="package-release-notes">
                 <h3>Release Notes - <?php echo $context->version['release']; ?></h3>
                 <p>
-                    <?php echo nl2br(trim($context->notes)); ?>
+                <?php
+                $markdown = new \PEAR2\Text\Markdown_Extra();
+                echo $markdown->transform($context->notes);
+                ?>
                 </p>
             </div>
