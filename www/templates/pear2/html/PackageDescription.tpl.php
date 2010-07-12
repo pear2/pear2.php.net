29d239ddc07ec041a7099d094181c2287ed52d41
Michael Gauthier <gauthierm@php.net>
2010-07-12 20:57:21 +0000

    Don't nest paragraph tags. Markdown will insert them appropriately.

--- www/templates/pear2/html/PackageDescription.tpl.php
+++ www/templates/pear2/html/PackageDescription.tpl.php
@@ -1,16 +1,12 @@
-            <p>
-                <?php
-                $markdown = new \PEAR2\Text\Markdown_Extra();
-                echo $markdown->transform($context->getRaw('description'));
-                ?>
-            </p>
+            <?php
+            $markdown = new \PEAR2\Text\Markdown_Extra();
+            echo $markdown->transform($context->getRaw('description'));
+            ?>
 
             <div class="package-release-notes">
                 <h3>Release Notes - <?php echo $context->version['release']; ?></h3>
-                <p>
                 <?php
                 $markdown = new \PEAR2\Text\Markdown_Extra();
                 echo $markdown->transform($context->getRaw('notes'));
                 ?>
-                </p>
             </div>
