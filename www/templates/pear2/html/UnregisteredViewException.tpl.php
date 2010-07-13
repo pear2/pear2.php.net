742e8cae9f57dd549b3f1979650b48fa60c57485
Michael Gauthier <gauthierm@php.net>
2010-07-13 15:31:47 +0000

    Start of a PEAR2-specific 404 page.

---
+++ www/templates/pear2/html/UnregisteredViewException.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+// Send 404 header
+header('HTTP/1.0 404 Not Found');
+
+// Set the page title
+$parent->context->page_title = 'Page Not Found - '
+    . PEAR2\SimpleChannelFrontend\Main::$title;
+
+?>
+<h1>WHOAH Nelly.</h1>
+<p>That view doesn't exist!</p>
