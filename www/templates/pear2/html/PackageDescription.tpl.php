686df373fe1499d9d44da00ee93f15c0c5e24252
Brett Bieber <brett.bieber@gmail.com>
2011-03-03 16:35:41 +0000

    Re-use the same markdown class, instead of instantiating a new object.

--- www/templates/pear2/html/PackageDescription.tpl.php
+++ www/templates/pear2/html/PackageDescription.tpl.php
@@ -6,7 +6,6 @@
             <div class="package-release-notes">
                 <h3>Release Notes - <?php echo $context->version['release']; ?></h3>
                 <?php
-                $markdown = new \PEAR2\Text\Markdown_Extra();
                 echo $markdown->transform($context->getRaw('notes'));
                 ?>
             </div>
