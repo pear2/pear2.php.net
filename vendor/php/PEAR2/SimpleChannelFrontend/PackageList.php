5dd430ecb1e1592da26391bd6af16ed0f74a7b15
Brett Bieber <brett.bieber@gmail.com>
2011-05-04 08:25:44 -0500

    Merge minor fix from SCF project.

--- vendor/php/PEAR2/SimpleChannelFrontend/PackageList.php
+++ vendor/php/PEAR2/SimpleChannelFrontend/PackageList.php
@@ -2,17 +2,6 @@
 namespace PEAR2\SimpleChannelFrontend;
 class PackageList extends \PEAR2\Pyrus\Channel\RemotePackages
 {
-    public $directory;
-    
-    public $packages = array();
-    
-    /**
-     * Remote packages object
-     * 
-     * @var \PEAR2\Pyrus\Channel\RemotePackages
-     */
-    protected $_remotepackages;
-    
     function __construct($options = array())
     {
         parent::__construct($options['frontend']::$channel);
