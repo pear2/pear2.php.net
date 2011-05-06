d9d4a43576d40ea47c5b3225f8cc134e922b84ed
Michael Gauthier <mike@silverorange.com>
2011-05-06 15:35:46 -0300

    Update sample config.

--- config.sample.php
+++ config.sample.php
@@ -1,10 +1,13 @@
 <?php
+
 ini_set('display_errors', true);
 error_reporting(E_ALL);
+
 // set up basics
 require_once __DIR__ . '/vendor/php/PEAR2/Autoload.php';
 
-PEAR2\SimpleChannelFrontend\Main::$url   = 'http://pear2.php.net/';
-PEAR2\SimpleChannelFrontend\Main::$title = 'PEAR2';
+// \PEAR2Web\Config::$options['channel_path'] = '/home/my-user/my-channel';
+// \PEAR2Web\Config::$options['url']          = 'http://pear2.php.net/';
+// \PEAR2Web\Config::$options['title']        = 'PEAR2';
 
 $config = PEAR2\Pyrus\Config::singleton('/tmp/pear2web');
