bec7bb30df15cbbee46be8d90cf7f9a0b94f8298
Brett Bieber <brett.bieber@gmail.com>
2011-05-07 17:00:21 -0500

    Simple issue integration with the github api. Use JS so the client actually grabs the open and closed issue count.

--- www/templates/pear2/html/PackageBugs.tpl.php
+++ www/templates/pear2/html/PackageBugs.tpl.php
@@ -1,1 +1,16 @@
-<?php echo $context->getBugs(); ?>
+<a href="https://github.com/pear2/<?php echo $parent->context->name; ?>/issues" class="package-bugs-open">open bugs</a>,
+<a href="https://github.com/pear2/<?php echo $parent->context->name; ?>/issues?state=closed" class="package-bugs-closed">closed</a>
+<a href="https://github.com/pear2/<?php echo $parent->context->name; ?>/issues/new" class="package-bugs-new button button-small">Report New</a>
+<script type="text/javascript">
+$.getJSON('http://github.com/api/v2/json/issues/list/pear2/<?php echo $parent->context->name; ?>/open?callback=?',
+        function(data){
+            $('.package-bugs-open').html(data.issues.length + ' open');
+        }
+);
+$.getJSON('http://github.com/api/v2/json/issues/list/pear2/<?php echo $parent->context->name; ?>/closed?callback=?',
+        function(data){
+            $('.package-bugs-closed').html(data.issues.length + ' closed');
+        }
+);
+
+</script>
