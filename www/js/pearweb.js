3ab57a4d654a3cd92d8d577137b1a5b0a4bc70f9
Brett Bieber <brett.bieber@gmail.com>
2011-06-13 20:52:58 -0500

    Add HTML5 history pushState support when navigating the release files.

--- www/js/pearweb.js
+++ www/js/pearweb.js
@@ -1,16 +1,56 @@
+
+function changeContents(url) {
+	//first, set the height of maincontent to fix jumping
+	$('#maincontent').css({'height' : $('#maincontent').height()});
+    //$('#maincontent').html('<div class="wdn_loading"><img src="/wdn/templates_3.0/css/header/images/colorbox/loading.gif" /></div>');
+    $.get(
+        url+"?format=partial",
+        function(data) {
+            $('#maincontent').html(data);
+            $('#maincontent').css({'height' : 'auto'});
+            $('pre code').each(function(i, e) {hljs.highlightBlock(e, '    ')});
+        }
+    );
+}
+
 $(document).ready(function() {
 	// select input content when focused
 	$('input[type=text],input[type=password],input[type=search]')
 		.focus(function(e) {
 			this.select();
 		});
-	$('a.partial').click(function(){
-		$.get(this.href+'?format=partial', function(data, textStatus){
-			if (textStatus == 'success') {
-				$('#maincontent').html(data);
-				// @todo push the url onto the history stack
-			}
-		});
+	$('a.partial').live('click', function(event){
+		var url = this.href;
+	    var direction = {'right': "+=1000px"};
+	    if (this.id == 'next') {
+	    	direction = {'right': "+=1000px"};
+	    } else {
+	    	// figure out how to determine which way to go
+	    }
+	    
+	    $("#maincontent").animate(direction, "fast", function(){
+	        if (supports_history()) {
+	            var title = url.split(window.url);
+	            window.History.pushState(null, title, url);
+	        } else {
+	        	changeContents(url);
+	        }
+	    });
 		return false;
 	});
 });
+
+function supports_history() {
+	return !!(window.history && history.pushState)
+}
+
+//Smart browser history for HTML 5 browsers.
+(function(window,undefined){
+	if (supports_history()) {
+		var History = window.History;
+		$(window).bind('statechange',function(){
+			var State = History.getState();
+			changeContents(State.url);
+		});
+	}
+})(window);
